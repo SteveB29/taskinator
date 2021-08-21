6907bb6eb5c70f3c9f8e985a6d86797a6546101d
Steve Bendrick <steven.bendrick@gmail.com>
2021-08-21 11:34:45 -0400

    tasks load from localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -228,6 +228,55 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+
+// Get tasks items from localStorage
+// Convert tasks from the string format back into an array of object
+// interate through a tasks array and creates task elements on the page from it
+var loadTasks = function() {
+  tasks = localStorage.getItem("tasks");
+  
+  if (!tasks) {
+    tasks = [];
+    return false;
+  }
+
+  tasks = JSON.parse(tasks);
+
+  for (var i = 0; i < tasks.length; i++) {
+    tasks[i].id = taskIdCounter;
+    console.log(tasks[i]);
+    
+    // creates li element
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.setAttribute("data-task-id", tasks[i].id);
+    
+    // create a div element
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    var taskActionsEl = createTaskActions(tasks[i].id);
+    listItemEl.appendChild(taskActionsEl);
+
+    if (tasks[i].status === "to do") {
+      listItemEl.querySelector("select[name='status-change']").selectedIndex = "0";
+      tasksToDoEl.appendChild(listItemEl);
+    }
+    else if (tasks[i].status === "in progress") {
+      listItemEl.querySelector("select[name='status-change']").selectedIndex = "1";
+      tasksInProgressEl.appendChild(listItemEl);
+    }
+    else if (tasks[i].status === "complete") {
+      listItemEl.querySelector("select[name='status-change']").selectedIndex = "2";
+      tasksInProgressEl.appendChild(listItemEl);
+    }
+
+    taskIdCounter++;
+  }
+}
+
 // Create a new task
 formEl.addEventListener("submit", taskFormHandler);
 
@@ -236,3 +285,6 @@ pageContentEl.addEventListener("click", taskButtonHandler);
 
 // for changing the status
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
+
+// load localstorage when page is reloaded
+loadTasks();
