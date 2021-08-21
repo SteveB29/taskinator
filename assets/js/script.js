31ee6ea854ef5d89eacf7b949c7ba5ce9f22610a
Steve Bendrick <steven.bendrick@gmail.com>
2021-08-20 22:22:58 -0400

    added ability to change columns

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIDCounter = 0;
 var pageContentEl = document.querySelector("#page-content");
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
 
 var taskFormHandler = function(event) {
   event.preventDefault();
@@ -97,7 +99,7 @@ var createTaskActions = function(taskId) {
   actionContainerEl.appendChild(statusSelectEl);
 
   // add options to dropdown
-  var statusChoices = ["To Do", "In Progress", "Competed"];
+  var statusChoices = ["To Do", "In Progress", "Completed"];
   for (var i = 0; i < statusChoices.length; i++) {
     // create option element
     var statusOptionEl = document.createElement("option");
@@ -165,3 +167,26 @@ var editTask = function(taskId) {
 
   formEl.setAttribute("data-task-id", taskId);
 };
+
+var taskStatusChangeHandler = function(event) {
+  // get the task item's id
+  var taskId = event.target.getAttribute("data-task-id");
+
+  // get the currently selected option's value and convert to lowercase
+  var statusValue = event.target.value.toLowerCase();
+
+  // find the parent task item element based on the id
+  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']")
+
+  if (statusValue === "to do") {
+    tasksToDoEl.appendChild(taskSelected);
+  } 
+  else if (statusValue === "in progress") {
+    tasksInProgressEl.appendChild(taskSelected);
+  } 
+  else if (statusValue === "completed") {
+    tasksCompletedEl.appendChild(taskSelected);
+  }
+};
+
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
